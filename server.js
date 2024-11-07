2ee15200e19f60a76a0343d0e66d74cf4bc4523e
Malika <elabiadmalika64@gmail.com>
2024-11-08 00:29:20 +0100

    Ajout d'autre api

--- server.js
+++ server.js
@@ -3,18 +3,70 @@ const app = express();
 const fs = require('fs');
 const PORT =3000;
 
-app.listen(PORT,()=>{
-    console.log(`Le serveur est lancé sur le port ${PORT}`);
-});
-
-
-app.get('/api/products', (req, res) => {
-    fs.readFile('./data.json',  (err, data) => {
+// Laitiers
+app.get('/api/laitiers', (req, res) => {
+    fs.readFile('./laitiers.json',  (err, data) => {
         if (err) {
             console.error("Erreur lors de la lecture du fichier JSON", err);
             res.status(500).send("Erreur interne du serveur");
             return;
         }
-        res.json(JSON.parse(data));
+        res.status(200).json(JSON.parse(data));
     });
 });
+// Produit Menage
+app.get('/api/ProduitMenage', (req, res) => {
+    fs.readFile('./ProduitMenage.json',  (err, data) => {
+        if (err) {
+            console.error("Erreur lors de la lecture du fichier JSON", err);
+            res.status(500).send("Erreur interne du serveur");
+            return;
+        }
+        res.status(200).json(JSON.parse(data));
+    });
+});
+
+// Charcuterie
+
+app.get('/api/charcuterie', (req, res) => {
+    fs.readFile('./charcuterie.json',  (err, data) => {
+        if (err) {
+            console.error("Erreur lors de la lecture du fichier JSON", err);
+            res.status(500).send("Erreur interne du serveur");
+            return;
+        }
+        res.status(200).json(JSON.parse(data));
+    });
+});
+
+// Viande
+
+app.get('/api/viande', (req, res) => {
+    fs.readFile('./viande.json',  (err, data) => {
+        if (err) {
+            console.error("Erreur lors de la lecture du fichier JSON", err);
+            res.status(500).send("Erreur interne du serveur");
+            return;
+        }
+        res.status(200).json(JSON.parse(data));
+    });
+});
+
+// Legume Fruit
+
+app.get('/api/legumeFruit', (req, res) => {
+    fs.readFile('./legumeFruit.json',  (err, data) => {
+        if (err) {
+            console.error("Erreur lors de la lecture du fichier JSON", err);
+            res.status(500).send("Erreur interne du serveur");
+            return;
+        }
+        res.status(200).json(JSON.parse(data));
+    });
+});
+
+app.listen(PORT,()=>{
+    console.log(`Le serveur est lancé sur le port ${PORT}`);
+});
+
+
