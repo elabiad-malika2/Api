1c6c8f5790aacbb9ad4570013494386c2e5c3a37
Malika <elabiadmalika64@gmail.com>
2024-11-13 20:50:37 +0100

    data

--- server.js
+++ server.js
@@ -4,58 +4,8 @@ const fs = require('fs');
 const PORT =3000;
 
 // Laitiers
-app.get('/api/laitiers', (req, res) => {
-    fs.readFile('./laitiers.json',  (err, data) => {
-        if (err) {
-            console.error("Erreur lors de la lecture du fichier JSON", err);
-            res.status(500).send("Erreur interne du serveur");
-            return;
-        }
-        res.status(200).json(JSON.parse(data));
-    });
-});
-// Produit Menage
-app.get('/api/ProduitMenage', (req, res) => {
-    fs.readFile('./ProduitMenage.json',  (err, data) => {
-        if (err) {
-            console.error("Erreur lors de la lecture du fichier JSON", err);
-            res.status(500).send("Erreur interne du serveur");
-            return;
-        }
-        res.status(200).json(JSON.parse(data));
-    });
-});
-
-// Charcuterie
-
-app.get('/api/charcuterie', (req, res) => {
-    fs.readFile('./charcuterie.json',  (err, data) => {
-        if (err) {
-            console.error("Erreur lors de la lecture du fichier JSON", err);
-            res.status(500).send("Erreur interne du serveur");
-            return;
-        }
-        res.status(200).json(JSON.parse(data));
-    });
-});
-
-// Viande
-
-app.get('/api/viande', (req, res) => {
-    fs.readFile('./viande.json',  (err, data) => {
-        if (err) {
-            console.error("Erreur lors de la lecture du fichier JSON", err);
-            res.status(500).send("Erreur interne du serveur");
-            return;
-        }
-        res.status(200).json(JSON.parse(data));
-    });
-});
-
-// Legume Fruit
-
-app.get('/api/legumeFruit', (req, res) => {
-    fs.readFile('./legumeFruit.json',  (err, data) => {
+app.get('/api/data', (req, res) => {
+    fs.readFile('./data.json',  (err, data) => {
         if (err) {
             console.error("Erreur lors de la lecture du fichier JSON", err);
             res.status(500).send("Erreur interne du serveur");
@@ -69,4 +19,14 @@ app.listen(PORT,()=>{
     console.log(`Le serveur est lancé sur le port ${PORT}`);
 });
 
+// API laitiers :
+    // https://api-pr0m.onrender.com/api/laitiers
+// API Produit Menage :
+    // https://api-pr0m.onrender.com/api/ProduitMenage
+// API charcuterie:
+    //  https://api-pr0m.onrender.com/api/charcuterie
+// API viande:
+    //  https://api-pr0m.onrender.com/api/viande
 
+// API Legume Fruit:
+    //  https://api-pr0m.onrender.com/api/legumeFruit
